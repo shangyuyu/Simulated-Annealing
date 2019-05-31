e6424b891aaf520fc0aae68b7a7844c3e21cbe09
Ziyi <shangyuyu@sjtu.edu.cn>
2019-05-31 22:25:26 +0800

    Draft

--- Initialization.m
+++ Initialization.m
@@ -1,7 +1,20 @@
 % Initialize cities and canvas
 
-N = 30;
+N = 30;  % Number of cities
 
 % New canvas
-cityList = rand(N, 2);
+city = rand(N, 2);
+visitOrder = 1:N;
+global distCity
+distCity = zeros(N, N);
 
+for i = 1:N
+    for j = 1:N
+    
+        distCity(i, j) = sqrt((city(i, 1)-city(j, 1))^2 + (city(i, 2)-city(j, 2))^2);
+    end
+end
+
+figure;
+plot(city(:, 1), city(:, 2), 'LineStyle', 'none', 'Marker', 'o');
+hold on;
